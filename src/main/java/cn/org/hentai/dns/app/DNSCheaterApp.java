9f6ab623ea31a64c3e334e68f205971cff7cc35d
wangcheng <gates@mucang.cn>
2019-05-15 16:45:09 +0800

    删除不必要的annotation

--- src/main/java/cn/org/hentai/dns/app/DNSCheaterApp.java
+++ src/main/java/cn/org/hentai/dns/app/DNSCheaterApp.java
@@ -10,21 +10,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
-import org.sqlite.SQLiteDataSource;
-
-import javax.sql.DataSource;
 
 /**
  * Created by matrixy on 2019/4/19.
  */
 @ComponentScan(value = {"cn.org.hentai"})
-@EnableAutoConfiguration
 @SpringBootApplication
 @MapperScan("cn.org.hentai.dns")
 public class DNSCheaterApp
